9ca81700d7d5ee3e8ba641d8a7fd02e9fd429bfe
Edgar Guzman <edgar.guzman6009@alumnos.udg.mx>
2020-10-14 18:00:32 -0500

    Prueba01

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var n, aux int
+	result := 0
+
+	fmt.Scan(&n);
+	s := make([]int, 0, n)
+
+	for i:=0; i<n; i++ {
+		fmt.Scan(&aux);
+		s = append(s, aux);
+	}
+	for i:=0; i<len(s); i++ {//Es más rapido en el for anterior
+		result += s[i];
+	}
+	fmt.Println(result)
 }
